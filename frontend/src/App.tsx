a8ebd75298344a7a88f6f3791d6a76e375f5c9e8
ValdenirRomanJunior <vromanjunior@outlook.com>
2021-09-07 11:34:25 -0300

    components basicos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,17 @@
-function App(){
+import DataTable from "components/DataTable";
+import Footer from "components/footer";
+import NavBar from "components/navbar";
+
+const App = () => {
     return (
-        <div> 
-            <h1 className="text-primary">Ola mundo</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <h1 className="text-primary">Ola mundo</h1>
+                <DataTable/>
+            </div>
+            <Footer />
+        </>
     );
 }
 
